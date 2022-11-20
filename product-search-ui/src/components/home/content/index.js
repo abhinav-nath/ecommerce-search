976b7c466f718e2a4a050e8e555ec5a294561352
Abhinav Nath <abhinavnath@ymail.com>
2022-11-21 00:53:46 +0530

    [UI] Display search results

--- product-search-ui/src/components/home/content/index.js
+++ product-search-ui/src/components/home/content/index.js
@@ -5,87 +5,34 @@ import { SearchResultsContext } from "../../../contexts/SearchResultsContext";
 
 const Content = () => {
   const { results } = useContext(SearchResultsContext);
-  console.log("Content - " + results.totalResults)
+  console.log("Content - " + results.totalResults);
 
-  return (
-    <div className="sui-layout-main">
-      <div className="sui-layout-main-header">
-        <div className="sui-layout-main-header__inner">
-          <div className="sui-paging-info">
-            Total results: <strong>{results.totalResults}</strong>
+  if (results.totalResults > 0)
+    return (
+      <div className="sui-layout-main">
+        <div className="sui-layout-main-header">
+          <div className="sui-layout-main-header__inner">
+            <div className="sui-paging-info">
+              Total Results - <strong>{results.totalResults}</strong>
+            </div>
           </div>
         </div>
+        <div className="sui-layout-main-body">
+          <ul className="sui-results-container">
+            {results.products.map((searchResult) => (
+              <li className="sui-result" key={searchResult.code}>
+                <div className="sui-result__header">
+                  <p className="sui-result__title">{searchResult.name}</p>
+                </div>
+                <div className="sui-result__body">
+                  <pre>{JSON.stringify(searchResult, null, 4)}</pre>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="sui-layout-main-body">
-        <ul className="sui-results-container">
-          <li className="sui-result">
-            <div className="sui-result__header">
-              <a className="sui-result__title sui-result__title-link"
-                 href="https://www.nps.gov/acad/index.htm"
-                 target="_blank"
-                 rel="noopener noreferrer">Acadia</a>
-            </div>
-            <div className="sui-result__body">
-              <ul className="sui-result__details">
-                <li>
-                  <span className="sui-result__key">id</span>
-                  {" "}
-                  <span className="sui-result__value">park_acadia</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">visitors</span>
-                  {" "}
-                  <span className="sui-result__value">3303393</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">acres=</span>
-                  {" "}
-                  <span className="sui-result__value">49057.36</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">date_established</span>{" "}
-                  <span className="sui-result__value">1919-02-26T06:00:00Z</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">description</span>
-                  {" "}
-                  <span className="sui-result__value">
-                    Covering most of Mount Desert Island and
-                    other coastal islands, Acadia features
-                    the tallest mountain on the Atlantic
-                    coast of the United States, granite
-                    peaks, ocean shoreline, woodlands, and
-                    lakes. There are freshwater, estuary,
-                    forest, and intertidal habitats.
-                  </span>
-                </li>
-                <li>
-                  <span className="sui-result__key">nps_link</span>
-                  {" "}
-                  <span className="sui-result__value">https://www.nps.gov/acad/index.htm</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">states</span>
-                  {" "}
-                  <span className="sui-result__value">Maine</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">title</span>
-                  {" "}
-                  <span className="sui-result__value">Acadia</span>
-                </li>
-                <li>
-                  <span className="sui-result__key">location</span>
-                  {" "}
-                  <span className="sui-result__value">44.35,-68.21</span>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Content;
