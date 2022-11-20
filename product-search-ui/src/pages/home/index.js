1e2d3d30885f02531292b07b28249b26c4f32551
Abhinav Nath <abhinavnath@ymail.com>
2022-11-21 01:02:30 +0530

    [UI] Use Pagination React Component

--- product-search-ui/src/pages/home/index.js
+++ product-search-ui/src/pages/home/index.js
@@ -4,6 +4,7 @@ import Content from "../../components/home/content";
 import "./styles.css";
 import SideBar from "../../components/home/sidebar";
 import { SearchResultsContext } from "../../contexts/SearchResultsContext";
+import Pagination from "rc-pagination";
 
 const Home = () => {
   const [results, setResults] = useState([]);
@@ -24,20 +25,7 @@ const Home = () => {
             </div>
           </div>
           <div className="sui-layout-main-footer">
-            <ul className="rc-pagination sui-paging" unselectable="unselectable">
-              <li title="Previous Page" className="rc-pagination-disabled rc-pagination-prev" aria-disabled="true">
-                <a className="rc-pagination-item-link" disabled=""></a>
-              </li>
-              <li title="1" className="rc-pagination-item rc-pagination-item-1 rc-pagination-item-active" tabIndex="0">
-                <a>1</a>
-              </li>
-              <li title="2" className="rc-pagination-item rc-pagination-item-2" tabIndex="0">
-                <a>2</a>
-              </li>
-              <li title="Next Page" tabIndex="0" className=" rc-pagination-next" aria-disabled="false">
-                <a className="rc-pagination-item-link"></a>
-              </li>
-            </ul>
+            <Pagination current={1} pageSize={5} total={results.totalResults}/>
           </div>
         </div>
       </SearchResultsContext.Provider>
